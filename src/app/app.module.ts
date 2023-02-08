2e47229830b706882bf4ab4a003ddf226160982d
Thomas Wagner <t-wagner@web.de>
2023-02-08 06:54:45 +0100

    first playable version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon';
+import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component'; 
+import { MatDialogModule } from '@angular/material/dialog'; 
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { GameInfoComponent } from './game-info/game-info.component';
+import {MatCardModule} from '@angular/material/card'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartScreenComponent,
+    GameComponent,
+    PlayerComponent,
+    DialogAddPlayerComponent,
+    GameInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-add-player',
+  templateUrl: './dialog-add-player.component.html',
+  styleUrls: ['./dialog-add-player.component.scss']
+})
+export class DialogAddPlayerComponent implements OnInit {
+  name: string = '';
+
+  constructor(public dialogRef: MatDialogRef<DialogAddPlayerComponent>) {
+    
+  }
+  
+  ngOnInit(): void {
+    
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
